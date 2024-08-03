4d17addec5768b49954c47577279db2c338afc42
John <johnolshea@gmail.com>
2024-08-03 04:32:17 +0000

    Remaining tasks

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-"""
-Basic Annotations - add
-Function to write a type-annotated function add
-Takes two float a, b and return their sum as float
-"""
+'''Task 0's module.
+'''
 
 
 def add(a: float, b: float) -> float:
-    """
-    type-annotated function
-    """
+    '''Adds two floating-point numbers.
+    '''
     return a + b
